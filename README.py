f0af1faf470675357faa38cdfa7c4dd7b65e6d0e
Vivienzou <zoul@uw.edu>
2013-04-23 18:34:26 -0700

    Update README.py

--- README.py
+++ README.py
@@ -1,27 +1,15 @@
 code
 ====
 def f(a,b):
-  
   c = (a**2+b**2)**0.5
-  
   sinb = b/c
-  
   cosb = a/c
-  
-  tanb = a/b
-
+  tanb = a/c
   cosa = sinb
-  
-  sina = cosb
-  
+  sina = cosb  
   tana = 1/tanb
-
-  print("This program is working for Trig functions given by 2 sides a and b")
-  
+  print("This program is working for Trig functions given by 2 sides a and b")  
   print("The sin, cos and tan of alpha bounded by side b and c, in order, is")
-  
   print(sinb, cosb, tanb)
-
   print("The sin, cos and tan of Beta bounded by side a and c, in order, is")
-  
   print(sina, cosa, tana)
